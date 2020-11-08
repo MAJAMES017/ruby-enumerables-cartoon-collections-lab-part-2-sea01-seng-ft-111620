a59ef9cc2296b7228f59879ae235146539849177
michael james <majames071@gmail.com>
2020-11-08 21:41:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,4 +39,3 @@ def find_valid_calls(array)
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
-
